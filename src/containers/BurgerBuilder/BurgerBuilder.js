b51447deba7401369bfd20b1646bfff0cc72628c
Danny <contact@danfoster.io>
2018-03-01 12:12:44 -0600

    Map over the ingredients stored in the BurgerBuilder state to dynamically generate the Burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,14 +8,14 @@ class BurgerBuilder extends Component {
     ingredients: {
       salad: 1,
       bacon: 1,
-      cheese: 2,
+      cheese: 1,
       meat: 2
     }
   }
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Aux>
     );
