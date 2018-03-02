29f6b545f0364fe1db44f1f9ce05923046aab498
Danny <contact@danfoster.io>
2018-03-02 14:41:57 -0600

    Added life cycle hooks on the Modal component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(igKey => {
-      return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}</li>;
-    });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Your burger has the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><b>Total Price: {props.price.toFixed(2)}</b></p>
-      <p>Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
-
-export default orderSummary;
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('[OrderSummary] WillUpdate');
+  }
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(igKey => {
+        return <li key={igKey}><span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}</li>;
+      });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Your burger has the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><b>Total Price: {this.props.price.toFixed(2)}</b></p>
+        <p>Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    )
+  }
+}
+export default OrderSummary;
